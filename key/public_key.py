b3cee953d67c9a0f05241517aaa6dff76820f5d9
Marcos Barbosa <enos_mm@hotmail.com>
2018-02-13 17:50:54 -0300

    Classes to generate keys

--- key/public_key.py
+++ key/public_key.py
@@ -6,11 +6,14 @@ class PublicKey(object):
     
     def generate_key(self):
         # This is your private signature.
-        signed_key = ecdsa.SigningKey.from_string(self.private_key.decode("hex"), curve = ecdsa.SECP256k1)
+        self.signed_key = ecdsa.SigningKey.from_string(self.private_key.decode("hex"), curve = ecdsa.SECP256k1)
 
         # This is a public verification key.
-        verification_key = signed_key.verifying_key
+        verification_key = self.signed_key.verifying_key
 
         # Generating your public key based on your verification key.
         return ('\04' + verification_key.to_string()).encode("hex")
+
+    def sign_msg(self, hashed_rtx)  :
+        return self.signed_key.sign_digest(hashed_rtx, sigencode = ecdsa.util.sigencode_der)
     pass
